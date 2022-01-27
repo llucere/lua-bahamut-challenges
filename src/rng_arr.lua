daec897e0925ddd0664325f82dfa02f2d686d076
llucere <66846791+llucere@users.noreply.github.com>
2022-01-27 16:51:51 +0200

    Update rng_arr.lua

--- src/rng_arr.lua
+++ src/rng_arr.lua
@@ -1,9 +1,3 @@
-function table.find(tbl, val)
-	for _, v in ipairs(tbl) do
-		if (v == val) then return true end
-	end
-end
-
 local function getRandom(arr, arr2, ind)
 	local int = math.random(1, 10)
 	while (table.find(arr, int) or (arr2 and (arr2[ind] == int))) do
@@ -14,26 +8,17 @@ end
 
 local function getNewArray()
 	local array = {}
-	for _ = 1, 10 do
-		table.insert(array, getRandom(array))
-	end
+	for _ = 1, 10 do table.insert(array, getRandom(array)) end
 	return array
 end
 
 local function getSecondArray(array)
 	local array2 = {}
-	for i = 1, 10 do
-		table.insert(array2, getRandom(array2, array, i))
-	end
+	for i = 1, 10 do table.insert(array2, getRandom(array2, array, i)) end
 	return array2
 end
 
 local na = getNewArray()
-for i = 1, 10 do
-	io.write(na[i], " ")
-end
-
+print(unpack(na))
 local sa = getSecondArray(na)
-for i = 1, 10 do
-	io.write(sa[i], " ")
-end
+print(unpack(sa))
